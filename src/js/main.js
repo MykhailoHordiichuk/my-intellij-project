34cba98827baf26c94c2d6b7668ea0ac3ecaf41e
kamayoti <kostya.karaman44@gmail.com>
2025-07-21 23:18:14 +0200

    change title for registration form

--- src/js/main.js
+++ src/js/main.js
@@ -44,3 +44,116 @@
       }, 5000);
     });
   });
+
+
+
+  // login form
+
+const loginForm = document.getElementById('login');
+const resultSignIn = document.getElementById('result_signIn');
+
+loginForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const formData = new FormData(loginForm);
+  const data = {
+    email: formData.get('email'),
+    password: formData.get('password')
+  };
+
+  fetch('http://localhost:8080/api/auth/login	', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Basic ' + btoa('michael:qwerty') // если требуется basic auth
+    },
+    body: JSON.stringify(data)
+  })
+  .then(async res => {
+    const responseBody = await res.text(); // читаем как текст (или res.json() если точно JSON)
+    
+    if (!res.ok) {
+      throw new Error(responseBody || `Login failed: ${res.status}`);
+    }
+
+    resultSignIn.textContent = 'Login successful!';
+    resultSignIn.className = 'response-msg success';
+    loginForm.reset();
+
+    setTimeout(() => {
+      resultSignIn.textContent = '';
+      resultSignIn.className = 'response-msg';
+    }, 5000);
+  })
+  .catch(err => {
+    console.error(err);
+    resultSignIn.textContent = 'Ошибка: ' + err.message;
+    resultSignIn.className = 'response-msg error';
+
+    setTimeout(() => {
+      resultSignIn.textContent = '';
+      resultSignIn.className = 'response-msg';
+    }, 5000);
+  });
+});
+
+
+
+
+
+
+// create account form
+
+
+const create_account_form = document.getElementById('create_account_form');
+const restul_create_user = document.getElementById('result_create_account');
+
+
+
+create_account_form.addEventListener('submit', function(e){
+
+  e.preventDefault();
+  const formData = new FormData(create_account_form);
+
+  const data = {
+    username: formData.get('username'),
+    email: formData.get('email'),
+    password: formData.get('password'),
+    fullName: formData.get('fullname')
+  };
+
+ fetch('http://localhost:8080/api/auth/register	', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Basic ' + btoa('michael:qwerty') // если требуется basic auth
+    },
+    body: JSON.stringify(data)
+  })
+  .then(async res => {
+    const responseBody = await res.text(); // читаем как текст (или res.json() если точно JSON)
+    
+    if (!res.ok) {
+      throw new Error(responseBody || `Login failed: ${res.status}`);
+    }
+
+    restul_create_user.textContent = 'Login successful!';
+    restul_create_user.className = 'response-msg success';
+    create_account_form.reset();
+
+    setTimeout(() => {
+      restul_create_user.textContent = '';
+      restul_create_user.className = 'response-msg';
+    }, 5000);
+  })
+  .catch(err => {
+    console.error(err);
+    restul_create_user.textContent = 'Ошибка: ' + err.message;
+    restul_create_user.className = 'response-msg error';
+
+    setTimeout(() => {
+      restul_create_user.textContent = '';
+      restul_create_user.className = 'response-msg';
+    }, 5000);
+  });
+});
